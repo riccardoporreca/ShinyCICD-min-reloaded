63b2928812a9f6c1b2fb1eefd84d66527bd98fce
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-07-26 23:58:57 +0200

    Include histogram for eruptions (close #1)

--- R/app_server.R
+++ R/app_server.R
@@ -8,5 +8,7 @@ app_server <- function(input, output, session) {
 
   callModule(mod_faithful_histogram_server, "hist-waiting",
              reactive("waiting"))
+  callModule(mod_faithful_histogram_server, "hist-eruptions",
+             reactive("eruptions"))
 
 }
